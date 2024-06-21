db5d840cc58ff021148ce7e7ebfd03cb08b727fb
c-wide <53365073+c-wide@users.noreply.github.com>
2024-06-21 16:40:58 -0500

    refactor player history query and add alliance history query

--- query.sql
+++ query.sql
@@ -13,40 +13,95 @@ SELECT EXISTS(SELECT 1 FROM guilds WHERE guild_id = $1 AND region = $2);
 -- name: DoesAllianceExist :one
 SELECT EXISTS(SELECT 1 FROM alliances WHERE alliance_id = $1 AND region = $2);
 
--- name: GetPlayerHistory :one
-SELECT 
-  (
-    SELECT array_to_json(array_agg(row_to_json(g)))
-    FROM (
-      SELECT 
-        pgm.guild_id,
-        g.name,
-        pgm.first_seen,
-        pgm.last_seen,
-        (
-          SELECT array_to_json(array_agg(row_to_json(a)))
-          FROM (
-            SELECT 
-              gam.alliance_id,
-              a.name,
-              a.tag,
-              GREATEST(pgm.first_seen, gam.first_seen) AS first_seen,
-              LEAST(pgm.last_seen, gam.last_seen) AS last_seen
-            FROM 
-              guild_alliance_memberships gam
-            JOIN 
-              alliances a ON gam.alliance_id = a.alliance_id AND gam.region = a.region
-            WHERE 
-              gam.guild_id = g.guild_id AND gam.region = g.region
-              AND gam.first_seen <= pgm.last_seen
-              AND gam.last_seen >= pgm.first_seen
-          ) a
-        ) AS alliances
-      FROM 
-        player_guild_memberships pgm
-      JOIN 
-        guilds g ON pgm.guild_id = g.guild_id AND pgm.region = g.region
-      WHERE 
-        pgm.player_id = $1 AND pgm.region = $2
-    ) g
-  ) AS history;
+-- name: GetPlayerHistory :many
+SELECT
+	g.name,
+	g.guild_id,
+	pgm.first_seen,
+	pgm.last_seen,
+	COALESCE(
+	(
+	SELECT
+		array_to_json(array_agg(row_to_json(a)))
+	FROM
+		(
+		SELECT
+			a.alliance_id,
+			a.name,
+			a.tag,
+			GREATEST(pgm.first_seen, gam.first_seen) AS first_seen,
+			LEAST(pgm.last_seen, gam.last_seen) AS last_seen
+		FROM
+			guild_alliance_memberships gam
+		JOIN 
+            alliances a ON
+			gam.alliance_id = a.alliance_id
+			AND gam.region = a.region
+		WHERE
+			gam.guild_id = g.guild_id
+			AND gam.region = g.region
+			AND gam.first_seen <= pgm.last_seen
+			AND gam.last_seen >= pgm.first_seen
+		ORDER BY
+			gam.first_seen DESC
+		) a
+	),
+	'[]'::JSON
+	) AS alliances
+FROM
+	player_guild_memberships pgm
+JOIN
+	guilds g ON
+	pgm.guild_id = g.guild_id
+	AND pgm.region = g.region
+WHERE 
+	pgm.player_id = $1
+	AND pgm.region = $2
+ORDER BY 
+	pgm.first_seen DESC;
+
+-- name: GetAllianceHistory :many
+SELECT
+	g.name,
+	g.guild_id,
+	gam.first_seen,
+	gam.last_seen,
+	COALESCE(
+    (
+	SELECT
+		array_to_json(array_agg(row_to_json(p)))
+	FROM
+		(
+		SELECT
+			p.player_id,
+			p.name,
+			GREATEST(gam.first_seen, pgm.first_seen) AS first_seen,
+			LEAST(gam.last_seen, pgm.last_seen) AS last_seen
+		FROM
+			player_guild_memberships pgm
+		JOIN 
+            players p ON
+			pgm.player_id = p.player_id
+			AND pgm.region = p.region
+		WHERE
+			pgm.guild_id = g.guild_id
+			AND pgm.region = g.region
+			AND pgm.first_seen <= gam.last_seen
+			AND pgm.last_seen >= gam.first_seen
+		ORDER BY
+			pgm.first_seen DESC
+        ) p
+    ),
+	'[]'::JSON
+    ) AS players
+FROM
+	guild_alliance_memberships gam
+JOIN
+    guilds g ON
+	gam.guild_id = g.guild_id
+	AND gam.region = g.region
+WHERE
+	gam.alliance_id = $1 
+	AND gam.region = $2
+ORDER BY
+	gam.first_seen DESC;
